0125d2a791c7221e2c234277bf99e6fb7a80f7c3
Jesus Emmanuel Baca Hernandez <Jesus Emmanuel Baca Hernandez>
2016-10-23 21:27:14 -0500

    Primer intento

--- cpu.c
+++ cpu.c
@@ -2,9 +2,23 @@
 #include<stdlib.h>
 
 int main ()
-{
+{   int tem, temtotal, numtem=0;
+    float prom;
 
-
+    printf("\n\tPROGRAMA PARA PROMEDIO DE TEMPERATURAS DE CPU\n\n");
+    printf("Ingrese la primera temperatura: ");
+    scanf("%i",&tem);
+while(tem!=0)
+  {
+   tem+=temtotal;
+    numtem++;
+    printf("\nIngrese temperatura o ingrese CERO para finalizar: ");
+    scanf("%i",&tem);
+  }
+  prom=(float)temtotal/numtem;
+  printf("\nNumero de temperaturas ingresadas: %i",numtem);
+  printf("\nSuma de temperaturas: %i",temtotal);
+  printf("\nPromedio de las temperaturas: %f",prom);
 
 system("pause");
 return 0;
